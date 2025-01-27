f586334d348176f8746524e2bcedb159dd3078fc
anastasiakosh <anastasiakoseleva31@hg47.org>
2025-01-27 20:51:00 +0200

    Updated App.java with latest changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+//32*32 px (18*20)
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight= columnCount * tileSize;
+        
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
